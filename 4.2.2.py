34c1a789379f7ab5a203b9fb7cc58b7c62c6cab3
tr03adam <tr03adam@gmail.com>
2026-02-23 22:37:56 +0200

    Revert ""[add] 3""
    This reverts commit 66ce529fe87b9f9ff920d3bafd9537ca9c1fca24.

--- 4.2.2.py
+++
@@ -1,7 +0,0 @@
-word = input("Enter a word: ")
-cleanWord = word.replace(" ", "").lower()
-print (cleanWord)
-if cleanWord == cleanWord[::-1]:
-    print ("OK")
-else:
-    print ("NOT")
--- 4.3.py
+++
@@ -1,8 +0,0 @@
-guess = input("Guess a letter: ").lower()
-
-if len(guess)>1 and guess.isalpha():
-    print ("E1")
-elif len(guess) == 1 and not guess.isalpha():
-    print ("E2")
-elif len(guess)>1 and not guess.isalpha():
-    print ("E3")
